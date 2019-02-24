5b4a5b790bd41792fd9e28762dff9ae67eb84ddc
Axel Greavette <vultureavatar@gmail.com>
2019-02-24 16:09:54 -0500

    Update commit.js

--- src/front-end/assets/js/commit.js
+++ src/front-end/assets/js/commit.js
@@ -4,8 +4,8 @@ xhttp.onreadystatechange = function() {
     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     var sha = JSON.parse(this.response)[0].sha.substring(0, 7)
     var date = new Date(JSON.parse(this.response)[0].commit.author.date).toLocaleDateString("en-CA", options()
-    document.getElementById("latest-commit-sha").innerHTML = '<a href=https://github.com/axelgreavette/axelgreavette.xyz/commit/' + sha + '>' + sha + '</a>'
-    document.getElementById("latest-commit-date").innerHTML = date
+    document.getElementById("latest-commit-sha").innerHTML = '<strong><a href=https://github.com/axelgreavette/axelgreavette.xyz/commit/' + sha + '>' + sha + '</a></strong>'
+    document.getElementById("latest-commit-date").innerHTML = '<strong>' + date = '</strong>'
   }  
 }
 xhttp.open("GET", "https://api.github.com/repos/axelgreavette/axelgreavette.xyz/commits", true)
