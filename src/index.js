fc712599efef21a5a04ebd23ce5180cf31874842
Axel Greavette <vultureavatar@gmail.com>
2019-02-25 14:22:31 -0500

    This should add legacy support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+require("dotenv").config()
 const express = require('express')
 const app = express()
+const { join } = require("path")
+const subdomain = require("express-subdomain")
 
-app.use('/', express.static(__dirname + '/front-end'))
+const LegacyAbout = express.Router()
 
-app.listen(process.env.PORT || 8081, console.log("Site is up and running on 8081")) 
+app.use('/', express.static(join(__dirname, "front-end")))
+
+LegacyAbout.use("/legacy", express.static(join(__dirname, "about") ))
+
+app.use(subdomain("legacy", LegacyAbout))
+app.listen(process.env.PORT || 8081, console.log(`Site is up and running on ${process.env.PORT || 8081}`))
