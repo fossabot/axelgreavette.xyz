d7ad58a4388e7ba48fc861bd0e39085eb9b8153e
Axel Greavette <vultureavatar@gmail.com>
2019-02-28 23:27:42 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,18 +5,14 @@ const { join } = require("path")
 const Legacy = express.Router()
 const vhost = require("vhost")
 
-Legacy.all("/", express.static(join(__dirname, "legacy")))
+Legacy.use("/", express.static(join(__dirname, "legacy")))
 
 app.use(vhost("legacy.axelgreavette.xyz", Legacy))
 
-app.all('/', express.static(join(__dirname, "front-end")))
+app.use('/', express.static(join(__dirname, "front-end")))
 
 app.get("*", function(req, res, next) {
-    res.status(404).redirect("/oof/its/a/404")
-})
-
-app.get("/oof/its/a/404", function(req, res, next) {
-    res.send("Roses are red,\n Voilets arn't blue,\n This is a 404 page, \n It's not to be viewed by you")
+    res.status(404).send("Roses are red,\n Voilets arn't blue,\n This is a 404 page, \n It's not to be viewed by you")
 })
 
 app.listen(process.env.PORT || 8081, console.log(`Site is up and running on ${process.env.PORT || 8081}`))
