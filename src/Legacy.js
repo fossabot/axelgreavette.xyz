aa2fdb2772f8fa51afa488796cef0a06f7ad2270
Axel Greavette <vultureavatar@gmail.com>
2019-02-28 22:27:16 -0500

    Update Legacy.js

--- src/Legacy.js
+++ src/Legacy.js
@@ -3,7 +3,7 @@ const { path } = require("path")
 
 module.exports = (router, express, app) => {
 
-  router.use("/", express.static(path(__dirname, "legacy"))
+  router.use("/", express.static(path(__dirname, "legacy")))
   
   app.use(subdomain("legacy", router))
   
