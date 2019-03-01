4beafe2aa974a035cf20c89155320aa9f952dc5e
Axel Greavette <vultureavatar@gmail.com>
2019-02-28 22:39:29 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ const { join } = require("path")
 
 app.use('/', express.static(join(__dirname, "front-end")))
 
-require("./Legacy.js")(express.Router(), express, app)
+require("./Legacy.js")(express, app)
 app.listen(process.env.PORT || 8081, console.log(`Site is up and running on ${process.env.PORT || 8081}`))
