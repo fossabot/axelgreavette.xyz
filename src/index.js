6d884e7c9a046970bb4068f29f7f27f0669d35ff
Axel Greavette <vultureavatar@gmail.com>
2019-02-28 23:19:35 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,4 +11,12 @@ app.use(vhost("legacy.axelgreavette.xyz", Legacy))
 
 app.use('/', express.static(join(__dirname, "front-end")))
 
+app.get("*", function(req, res, next) {
+  res.redirect("/oof/its/a/404")
+})
+
+app.get("/oof/its/a/404", function(req, res, next) {
+    res.send("Roses are red,\n Voilets arn't blue,\n This is a 404 page, \n It's not to be viewed by you")
+})
+
 app.listen(process.env.PORT || 8081, console.log(`Site is up and running on ${process.env.PORT || 8081}`))
