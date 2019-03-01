85bd2f2437ac0603ee9ac8438dd9008d6583b59c
Axel Greavette <vultureavatar@gmail.com>
2019-02-28 22:26:29 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 require("dotenv").config()
-const subdomain = require("express-subdomain")
 const express = require('express')
 const app = express()
 const { join } = require("path")
 
-const Legacy = express.Router()
-
-Legacy.use("/", express.static(join(__dirname, "legacy")))
-app.use(subdomain("legacy", Legacy))
-
 app.use('/', express.static(join(__dirname, "front-end")))
 
+require("./Legacy.js")(express.Router(), express, app)
 app.listen(process.env.PORT || 8081, console.log(`Site is up and running on ${process.env.PORT || 8081}`))
