8d99d5468e2823e4e783a586479454cdf7867b07
Axel Greavette <vultureavatar@gmail.com>
2019-02-28 22:37:54 -0500

    Update Legacy.js

--- src/Legacy.js
+++ src/Legacy.js
@@ -1,8 +1,4 @@
-const vhost = require("vhost")
-const { join } = require("path")
-
 module.exports = (express, app) => {
 
-  app.use(vhost("legacy.axelgreavette.xyz", express.static(join(__dirname, "legacy"))))
-  
+  app.use("/legacy", express.static("legacy"))  
 }
