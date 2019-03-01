80af71a7d2fd51b7f8e46ed5966a6425d0f8ffa3
Axel Greavette <vultureavatar@gmail.com>
2019-02-28 22:24:19 -0500

    Create Legacy.js

---
+++ src/Legacy.js
@@ -0,0 +1,10 @@
+const subdomain = require("express-subdomain")
+const { path } = require("path")
+
+module.exports = (router, express, app) => {
+
+  router.use(express.static(path(__dirname, "legacy"))
+  
+  app.use(subdomain("legacy", router))
+  
+})
