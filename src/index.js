e9118a4969646637f1504dd61af2a3aa83c36752
Axel Greavette <vultureavatar@gmail.com>
2019-02-28 22:42:29 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 require("dotenv").config()
 const express = require('express')
 const app = express()
-const { join } = require("path")
 
-app.use('/', express.static(join(__dirname, "front-end")))
+app.use('/', express.static("front-end"))
 
 require("./Legacy.js")(express, app)
+
 app.listen(process.env.PORT || 8081, console.log(`Site is up and running on ${process.env.PORT || 8081}`))
