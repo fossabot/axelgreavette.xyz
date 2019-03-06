4ca3b90b251e00aa7fed833bb936be5d8b763880
Axel Greavette <vultureavatar@gmail.com>
2019-03-06 18:39:55 -0500

    Move to render engine

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ const app = express()
 const { join } = require("path")
 const Legacy = express.Router()
 const vhost = require("vhost")
+const Render = require("./util/Render")
+
+app.set('view engine', 'hbs');
 
 Legacy.use("/", express.static(join(__dirname, "legacy")))
 
 app.use(vhost("legacy.axelgreavette.xyz", Legacy))
 
 app.use('/', express.static(join(__dirname, "front-end")))
+
+app.get("/", function(req, res, next) {
+    res.render(join(__dirname, "front-end"), Render)
+})
+app.get("/about-the-site", function(req, res, next) {
+    res.render(join(__dirname, "front-end", "about-the-site"), Render)
+})
 
 app.get("*", function(req, res, next) {
     res.status(404).send("Roses are red,\n Voilets arn't blue,\n This is a 404 page, \n It's not to be viewed by you")
