fc838b539a87370b2c320c834c9d8fd9d5e0bd3f
Axel Greavette <vultureavatar@gmail.com>
2019-03-06 21:02:35 -0500

    Update Render.js

--- src/util/Render.js
+++ src/util/Render.js
@@ -2,5 +2,5 @@ module.exports = {
     "age": "14",
     "name": "Axel Greavette",
     "mail": "mailto:axelgreavette@gmail.com?subject=Job&nbsp;Offer",
-    "paypal": "https://www.paypal.me/axelgreavette/5.00"
+    "paypal": "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=MKJAGRH3FSD68&item_name=To+buy+a+Coca+Cola+with&currency_code=CAD&amount=5&source=url"
 }
