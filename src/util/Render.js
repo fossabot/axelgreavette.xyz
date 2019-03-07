db5e23fc87ecff96b4d6d0ba76144a35424e0925
Axel Greavette <vultureavatar@gmail.com>
2019-03-06 19:55:52 -0500

    Update Render.js

--- src/util/Render.js
+++ src/util/Render.js
@@ -1,5 +1,6 @@
 module.exports = {
     "age": "14",
     "name": "Axel Greavette",
-    "mail": "mailto:axelgreavette@gmail.com?subject=Job&nbsp;Offer"
+    "mail": "mailto:axelgreavette@gmail.com?subject=Job&nbsp;Offer",
+    "paypal": "https://www.paypal.me/axelgreavette/5.00"
 }
