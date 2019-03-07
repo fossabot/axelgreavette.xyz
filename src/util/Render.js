849a55aab1744a22198622dd757108b3748fc574
Axel Greavette <vultureavatar@gmail.com>
2019-03-06 21:11:53 -0500

    Update Render.js

--- src/util/Render.js
+++ src/util/Render.js
@@ -2,5 +2,7 @@ module.exports = {
     "age": "14",
     "name": "Axel Greavette",
     "mail": "mailto:axelgreavette@gmail.com?subject=Job&nbsp;Offer",
-    "paypal": "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=MKJAGRH3FSD68&item_name=To+buy+a+Coca+Cola+with&currency_code=CAD&amount=5&source=url"
+    "paypal": "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=MKJAGRH3FSD68&item_name=To+buy+a+Coca+Cola+with&currency_code=CAD&amount=5&source=url",
+    "discord_invite": "https://discord.gg/SjBs52S",
+    "patreon": "https://www.patreon.com/bePatron?u=10511182"
 }
